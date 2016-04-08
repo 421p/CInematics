6f71750dbb4f13617a99f157447fb7b47e9fedf2
421p <andrew.bakal@gmail.com>
2016-04-08 12:36:30 +0300

    JsonResponse instead of Response

--- src/Controllers/AjaxController.php
+++ src/Controllers/AjaxController.php
@@ -4,6 +4,7 @@ namespace Cinematics\Controllers;
 
 use Cinematics\DatabaseProvider;
 use Silex\ControllerCollection;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 class AjaxController
@@ -27,51 +28,51 @@ class AjaxController
 
     private function initRoutes()
     {
-        $this->router->get('/', function() {
+        $this->router->post('/', function() {
             return json_encode([
                 'Api Version' => '0.0.2',
                 'Author' => 'Mykola Prokopenko'
             ]);
         });
 
-        $this->router->match('/add/movie', function(Request $request) {
+        $this->router->post('/add/movie', function(Request $request) {
             $data = json_decode(urldecode($request->getQueryString()), true);
-            return json_encode(['what' => $this->model->addMovie($data)]);
+            return new JsonResponse(['what' => $this->model->addMovie($data)]);
         });
 
-        $this->router->match('/add/seance', function(Request $request) {
+        $this->router->post('/add/seance', function(Request $request) {
             $data = json_decode(urldecode($request->getQueryString()), true);
-            return json_encode(['what' => $this->model->addSeance($data)]);
+            return new JsonResponse(['what' => $this->model->addSeance($data)]);
         });
 
-        $this->router->match('/add/ticket', function(Request $request) {
+        $this->router->post('/add/ticket', function(Request $request) {
             $data = json_decode(urldecode($request->getQueryString()), true);
-            return json_encode(['what' => $this->model->sellTicket($data)]);
+            return new JsonResponse(['what' => $this->model->sellTicket($data)]);
         });
 
-        $this->router->match('/json', function(Request $request) {
+        $this->router->post('/json', function(Request $request) {
             $arr = json_decode(urldecode($request->getQueryString()), true);
-            return json_encode($arr);
+            return new JsonResponse($arr);
         });
 
-        $this->router->match('/halls', function() {
-            return json_encode($this->model->getHallsInfo(), JSON_NUMERIC_CHECK);
+        $this->router->post('/halls', function() {
+            return new JsonResponse($this->model->getHallsInfo(), JSON_NUMERIC_CHECK);
         });
 
-        $this->router->match('/halls/{name}', function($name) {
-            return json_encode($this->model->getHallsInfo($name), JSON_NUMERIC_CHECK);
+        $this->router->post('/halls/{name}', function($name) {
+            return new JsonResponse($this->model->getHallsInfo($name), JSON_NUMERIC_CHECK);
         });
 
-        $this->router->match('/seances/{id}', function($id) {
-            return json_encode($this->model->getSeanceInfo($id), JSON_NUMERIC_CHECK);
+        $this->router->post('/seances/{id}', function($id) {
+            return new JsonResponse($this->model->getSeanceInfo($id), JSON_NUMERIC_CHECK);
         });
 
-        $this->router->match('/seances/{from}/{to}', function($from, $to) {
-            return json_encode($this->model->getSeancesBetweenDates($from, $to), JSON_NUMERIC_CHECK);
+        $this->router->post('/seances/{from}/{to}', function($from, $to) {
+            return new JsonResponse($this->model->getSeancesBetweenDates($from, $to), JSON_NUMERIC_CHECK);
         });
 
-        $this->router->match('/movies', function() {
-            return json_encode($this->model->getMovies());
+        $this->router->post('/movies', function() {
+            return new JsonResponse($this->model->getMovies());
         });
     }
 }
